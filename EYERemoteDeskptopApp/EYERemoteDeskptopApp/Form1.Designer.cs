f2eacbf92a0eab685defd67ac92f151ea4b14ddf
volevu17 <121816250+volevu17@users.noreply.github.com>
2024-07-18 15:03:13 +0700

    Remote-Desktop

---
+++ EYERemoteDeskptopApp/EYERemoteDeskptopApp/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace EYERemoteDeskptopApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtYourIp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtClientIp = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btnConnect = new MetroFramework.Controls.MetroButton();
+            this.btnDisconnect = new MetroFramework.Controls.MetroButton();
+            this.rdp = new AxMSTSCLib.AxMsRdpClient9NotSafeForScripting();
+            this.lblClientsystemName = new System.Windows.Forms.Label();
+            this.btnping = new MetroFramework.Controls.MetroButton();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rdp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel1.Controls.Add(this.txtYourIp);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(32, 79);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(408, 107);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtYourIp
+            // 
+            this.txtYourIp.Location = new System.Drawing.Point(21, 49);
+            this.txtYourIp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtYourIp.Multiline = true;
+            this.txtYourIp.Name = "txtYourIp";
+            this.txtYourIp.Size = new System.Drawing.Size(375, 31);
+            this.txtYourIp.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(24, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Your IP";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Red;
+            this.panel2.Location = new System.Drawing.Point(1, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(13, 105);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel3.Controls.Add(this.txtPassword);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.txtClientIp);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Location = new System.Drawing.Point(448, 79);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(740, 107);
+            this.panel3.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(293, 49);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(436, 31);
+            this.txtPassword.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(289, 26);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Password";
+            // 
+            // txtClientIp
+            // 
+            this.txtClientIp.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtClientIp.Location = new System.Drawing.Point(23, 49);
+            this.txtClientIp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtClientIp.Multiline = true;
+            this.txtClientIp.Name = "txtClientIp";
+            this.txtClientIp.Size = new System.Drawing.Size(261, 31);
+            this.txtClientIp.TabIndex = 4;
+            this.txtClientIp.Leave += new System.EventHandler(this.txtClientIp_Leave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(25, 23);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Client IP";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Red;
+            this.panel4.Location = new System.Drawing.Point(1, 0);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(13, 105);
+            this.panel4.TabIndex = 0;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(1197, 79);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(164, 108);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect Now";
+            this.btnConnect.UseSelectable = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(1369, 78);
+            this.btnDisconnect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(99, 108);
+            this.btnDisconnect.TabIndex = 5;
+            this.btnDisconnect.Text = "Disconect";
+            this.btnDisconnect.UseSelectable = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // rdp
+            // 
+            this.rdp.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rdp.Enabled = true;
+            this.rdp.Location = new System.Drawing.Point(33, 192);
+            this.rdp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdp.Name = "rdp";
+            this.rdp.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("rdp.OcxState")));
+            this.rdp.Size = new System.Drawing.Size(1328, 564);
+            this.rdp.TabIndex = 6;
+            // 
+            // lblClientsystemName
+            // 
+            this.lblClientsystemName.AutoSize = true;
+            this.lblClientsystemName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClientsystemName.Location = new System.Drawing.Point(471, 42);
+            this.lblClientsystemName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblClientsystemName.Name = "lblClientsystemName";
+            this.lblClientsystemName.Size = new System.Drawing.Size(0, 18);
+            this.lblClientsystemName.TabIndex = 7;
+            // 
+            // btnping
+            // 
+            this.btnping.Location = new System.Drawing.Point(1476, 78);
+            this.btnping.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnping.Name = "btnping";
+            this.btnping.Size = new System.Drawing.Size(201, 110);
+            this.btnping.TabIndex = 8;
+            this.btnping.Text = "Check Ping Status\r\nConnection Test";
+            this.btnping.UseSelectable = true;
+            this.btnping.Click += new System.EventHandler(this.btnping_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1796, 759);
+            this.Controls.Add(this.btnping);
+            this.Controls.Add(this.lblClientsystemName);
+            this.Controls.Add(this.rdp);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(27, 74, 27, 25);
+            this.Text = "  Remote Desktop Application";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rdp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtYourIp;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtClientIp;
+        private System.Windows.Forms.Label label2;
+        private MetroFramework.Controls.MetroButton btnConnect;
+        private MetroFramework.Controls.MetroButton btnDisconnect;
+        private AxMSTSCLib.AxMsRdpClient9NotSafeForScripting rdp;
+        private System.Windows.Forms.Label lblClientsystemName;
+        private MetroFramework.Controls.MetroButton btnping;
+    }
+}
+
---
+++ EYERemoteDeskptopApp/EYERemoteDeskptopApp/Form1.cs
@@ -0,0 +1,156 @@
+﻿using MetroFramework.Forms;
+using MSTSCLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EYERemoteDeskptopApp
+{
+    public partial class Form1 : MetroForm
+    {
+        bool pingable = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+               btnDisconnect.Enabled = false;
+                GetMyIp();
+                GetWindowName();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Error:"+ ex.Message, "some Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void GetWindowName()
+        {
+            //string MachineName1 = Environment.MachineName;
+
+
+        }
+
+        private void GetMyIp()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (var ip in host.AddressList)
+            {
+                if(ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    txtYourIp.Text = ip.ToString();
+                }
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(txtClientIp.Text.Trim() == string.Empty)
+                {
+                    MessageBox.Show("IP is Required", "Error ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtClientIp.Focus();
+                    return;
+                }
+                btnConnect.Enabled = false;
+                btnDisconnect.Enabled = true;
+
+                rdp.Server = txtClientIp.Text;
+                // rdp.UserName = txtUserName.Text;
+                rdp.UserName = "volev";
+
+
+                IMsTscNonScriptable Secured = (IMsTscNonScriptable)rdp.GetOcx();
+                Secured.ClearTextPassword = txtPassword.Text;
+                rdp.Connect();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+                }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            if (rdp.Connected.ToString() == "1")
+                rdp.Disconnect();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtClientIp_Leave(object sender, EventArgs e)
+        {
+            string MachineName = string.Empty;
+
+            try
+            {
+                string ip = txtClientIp.Text;
+                // MessageBox.Show(ip);
+                System.Net.IPHostEntry hostEntry = System.Net.Dns.GetHostEntry(ip);
+                MachineName = hostEntry.HostName;
+
+               lblClientsystemName.Text = "Client System Name"  + " " + MachineName;
+
+
+            }
+            catch (PingException)
+            {
+
+                throw;
+            }
+        }
+
+        private void btnping_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string ipAddress = txtClientIp.Text;
+                int port = 3389; // Port mặc định cho Remote Desktop Protocol (RDP)
+
+                using (TcpClient client = new TcpClient())
+                {
+                    var result = client.BeginConnect(ipAddress, port, null, null);
+                    var success = result.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(2));
+
+                    if (!success)
+                    {
+                        MessageBox.Show("Connection Timed Out", "Status", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    client.EndConnect(result);
+                    MessageBox.Show("Connection Success", "Status", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+    }
+}
